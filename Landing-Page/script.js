bb3076473f0e2ed1d9a562a98f71aeaae8e71372
Joshua Owusu <joshbillion1@gmail.com>
2023-07-19 07:00:10 +0000

    Commit auto_update

--- Landing-Page/script.js
+++ Landing-Page/script.js
@@ -87,3 +87,5 @@ function init() {
   // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
   ScrollTrigger.refresh();
 }
+
+init();
