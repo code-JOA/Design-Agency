7b73a04775ad01db48a7e9670bdb6c0364d9ecaa
Joshua Owusu <joshbillion1@gmail.com>
2023-07-20 12:10:35 +0000

    Commit auto_update

--- Landing-Page/script.js
+++ Landing-Page/script.js
@@ -64,18 +64,3 @@ t1.to(
 );
 
 t1.to(".page1 video", { width: "90%" }, "anim");
-
-// var t12 = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: ".page1 h1",
-//     scroller: ".main",
-//     markers: true,
-//     start: "top 127%",
-//     end: "top 0",
-//     scrub: 3,
-//   },
-// });
-
-.to(".main", {
-  backgroundColor: "#fff" 
-})
