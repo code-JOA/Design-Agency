4dd682a63ef34cddbd27cd2537e454eee0b11a81
Joshua Owusu <joshbillion1@gmail.com>
2023-07-23 07:32:01 +0000

    Commit auto_update

--- Landing-Page/script.js
+++ Landing-Page/script.js
@@ -97,20 +97,38 @@ tl2.to(".main", {
   backgroundColor: "#fff",
 });
 
-// var tl3 = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: ".page1 h1",
-//     scroller: ".main",
-//     // markers:true,
-//     start: "top -280%",
-//     end: "top -300%",
-//     scrub: 3,
-//   },
-// });
+var tl3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".page1 h1",
+    scroller: ".main",
+    // markers:true,
+    start: "top -280%",
+    end: "top -300%",
+    scrub: 3,
+  },
+});
 
-// tl3.to(".main", {
-//   backgroundColor: "#0F0D0D",
-// });
+tl3.to(".main", {
+  backgroundColor: "#0F0D0D",
+});
+
+var boxes = document.querySelectorAll(".box");
+boxes.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    var att = elem.getAttribute("data-image");
+    crsr.style.width = "470px";
+    crsr.style.height = "370px";
+    crsr.style.borderRadius = "0";
+    crsr.style.backgroundImage = `url(${att})`;
+  });
+  elem.addEventListener("mouseleave", function () {
+    elem.style.backgroundColor = "transparent";
+    crsr.style.width = "20px";
+    crsr.style.height = "20px";
+    crsr.style.borderRadius = "50%";
+    crsr.style.backgroundImage = `none`;
+  });
+});
 
 // function init() {
 //   gsap.registerPlugin(ScrollTrigger);
