aaa78765b5abeca832368eb729b8d35796e25468
Joshua Owusu <joshbillion1@gmail.com>
2023-07-25 09:54:16 +0000

    Commit auto_update

--- Landing-Page/script.js
+++ Landing-Page/script.js
@@ -38,7 +38,7 @@ init();
 // cursor
 var crsr = document.querySelector(".cursor");
 var main = document.querySelector(".main");
-main.addEventListener("mousemove", function (dets) {
+document.addEventListener("mousemove", function (dets) {
   crsr.style.left = dets.x + 20 + "px";
   crsr.style.top = dets.y + 20 + "px";
 });
